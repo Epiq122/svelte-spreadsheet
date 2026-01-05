117dc6e7a78317f1c3f19468452b30bdad327037
Robert Gleason <l2ob122@gmail.com>
2026-01-04 16:56:06 -0800

    feat(sheet): enhance cell editing functionality and styling

--- src/lib/components/sheet/Sheet.svelte
+++ src/lib/components/sheet/Sheet.svelte
@@ -2,26 +2,46 @@
 	import { numberToAlphabet, type Cell } from './sheet-utils';
 
 	let { data }: { data: Cell[][] } = $props();
+	let editedCell: string | null = $state(null);
+	let selectedCell: string | null = $state(null);
 
 	let numRows = $derived(data.length > 10 ? data.length : 10);
 	let numCols = $derived.by(() => {
 		const largestRow = Math.max(...data.map((row) => row.length));
 		return largestRow > 10 ? largestRow : 10;
 	});
+
+	function init(el: HTMLInputElement) {
+		el.focus();
+		el.select();
+	}
 </script>
 
-<table class="border-collapse font-sans">
+<table class="sheet">
 	<tbody>
 		{#each { length: numRows + 1 }, row}
 			<tr>
 				{#each { length: numCols + 1 }, column}
-					{@const cellData = data[row - 1]?.[column - 1]?.value}
+					{@const cell = data[row - 1]?.[column - 1]}
+					{@const cellData = cell?.value}
+					{@const currentCell = `${row}, ${column}`}
 					<svelte:element
 						this={row === 0 || column === 0 ? 'th' : 'td'}
-						class="box-border h-7.5 min-w-25 border border-[#393939] {row === 0 || column === 0
-							? 'bg-[#191919]'
-							: 'bg-[#222]'}"
+						class:selected={selectedCell === currentCell}
+						style:background-color={cell?.bgColor}
+						style:color={cell?.color}
 						scope={row === 0 ? 'col' : column === 0 ? 'row' : undefined}
+						role="button"
+						tabindex="0"
+						ondblclick={() => {
+							if (row === 0 || column == 0) return;
+							editedCell = currentCell;
+						}}
+						onclick={() => {
+							if (currentCell === selectedCell || row === 0 || column === 0) return;
+							selectedCell = currentCell;
+							editedCell = null;
+						}}
 					>
 						{#if row === 0 && column > 0}
 							{numberToAlphabet(column)}
@@ -30,11 +50,68 @@
 						{#if row > 0 && column === 0}
 							{row}
 						{/if}
-						{cellData || ''}
-						{#if row > 0 && column > 0}{/if}
+
+						{#if row > 0 && column > 0}
+							{#if editedCell !== currentCell}
+								{cellData || ''}
+							{:else}
+								<input
+									use:init
+									type="text"
+									value={cellData || ''}
+									style:background-color={cell?.bgColor}
+									style:color={cell?.color}
+									oninput={(e) => {
+										console.log(e.currentTarget.value);
+									}}
+								/>
+							{/if}
+						{/if}
 					</svelte:element>
 				{/each}
 			</tr>
 		{/each}
 	</tbody>
 </table>
+
+<style lang="scss">
+	.sheet {
+		border-collapse: collapse;
+		font-family: sans-serif;
+		* {
+			box-sizing: border-box;
+		}
+		tr {
+			th {
+				background-color: #191919;
+			}
+			td {
+				background-color: #222;
+			}
+			td.selected {
+				outline: 2px solid #3257f8;
+				outline-offset: -2px;
+			}
+			th,
+			td {
+				min-width: 100px;
+				height: 30px;
+				border: 1px solid #393939;
+				span {
+					padding: 5px;
+					display: inline-block;
+				}
+				input {
+					width: 100%;
+					height: 100%;
+					padding: 5px;
+					margin: 0;
+					border: none;
+					font-size: 16px;
+					background-color: #222;
+					color: #fff;
+				}
+			}
+		}
+	}
+</style>
